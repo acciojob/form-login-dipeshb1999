194679fc517804a1eceb6ab2e8e8aab761ed4ce0
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-23 18:21:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+// Handle form submission
+function getFormValue(event) {
+    event.preventDefault(); // Prevent form submission
 
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+
+    // Display the first and last name using alert()
+    alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
+
+// Add event listener to the form submission
+var form = document.getElementById("loginForm");
+form.addEventListener("submit", getFormValue);
